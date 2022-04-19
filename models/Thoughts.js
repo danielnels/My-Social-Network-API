40f997518ab463c42d28f6e431b3dbae5c25b1c2
danielnels <danieln@newground.net.au>
2022-04-19 11:05:55 +0800

    created models

---
+++ models/Thoughts.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const reactionsSchema = new Schema(
+  {
+ 
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,  
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
+    }
+  },
+  { toJSON: {getters: true,}}
+);
+
+// Schema to create thought Model
+const thoughtsSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,  
+      default: Date.now,
+      get: timestamp => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false  
+  }
+);
+
+thoughtsSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const usersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email:{
+      type: String,
+      unique: true,
+      required: true,
+      match: /.+\@.+\..+/, 
+    },
+    thoughts:[ 
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+usersSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const Users = model('Users', usersSchema);
+//exporting 
